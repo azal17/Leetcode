957fa08afddffa1bed020245c32df3084bed4d16
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-13 12:45:34 +0530

    Time: 665 ms (97.91%), Space: 27.4 MB (52.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -6,13 +6,13 @@ class Solution:
         if n <= 1:
             return 0  
         
-        min_price = float('inf') 
-        max_profit = 0
+        min_price = float('inf')
+        max_price = 0
+
+        for i in prices:
+            if i <min_price:
+                min_price = i
+            elif i - min_price>max_price:
+                max_price = i - min_price
         
-        for price in prices:
-            if price < min_price:
-                min_price = price  
-            elif price - min_price > max_profit:
-                max_profit = price - min_price  
-        
-        return max_profit
+        return max_price
