4a11d9c8b9e131f3b83982d06b497fc2b84b72cb
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-18 19:03:50 +0530

    Time: 101 ms (42.79%), Space: 18 MB (11.29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,5 +1,4 @@
-from typing import List
-
+#Boyer-Moore Voting Algorithm
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -8,36 +7,36 @@ class Solution:
         element1 = None
         element2 = None
 
-      
         for i in range(n):
-            if count1 == 0 and nums[i] != element2:
+            if count1 ==0 and nums[i] != element2:
                 element1 = nums[i]
-                count1 += 1
-            elif count2 == 0 and nums[i] != element1:
+                count1 +=1
+            elif  count2 ==0 and nums[i] != element1:
                 element2 = nums[i]
-                count2 += 1
+                count2 +=1
             elif element1 == nums[i]:
-                count1 += 1
+                count1 +=1
             elif element2 == nums[i]:
-                count2 += 1
+                count2 +=1
             else:
-                count1 -= 1
-                count2 -= 1
+                count1 -=1
+                count2 -=1
         
-      
         c1 = 0
         c2 = 0
         for i in range(n):
-            if nums[i] == element1:
-                c1 += 1
-            elif nums[i] == element2:
-                c2 += 1
+            if nums[i] ==element1:
+                c1 +=1
+            elif nums[i] ==element2:
+                c2 +=1
         
-        result = []
-        if c1 > n // 3:
-            result.append(element1)
-        if c2 > n // 3:
-            result.append(element2)
+        if c1>n/3 and c2>n/3:
+            return [element1,element2]
+        elif c1 > n/3:
+            return [element1]
+        elif c2> n/3:
+            return [element2]
+        return []
+
         
-        return result
 
