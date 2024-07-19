092f5c9ef1375e036e6e8949828a806d1ae3be34
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-19 20:27:05 +0530

    Time: 38 ms (39.81%), Space: 16.5 MB (27.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,11 @@
 class Solution:
-    def countUniquePaths(self, row: int, col: int, numRows: int, numCols: int, memo: List[List[int]]) -> int:
-        if row == (numRows - 1) and col == (numCols - 1):
-            return 1
-        if row >= numRows or col >= numCols:
-            return 0
-        if memo[row][col] != -1:
-            return memo[row][col]
-        else:
-            memo[row][col] = self.countUniquePaths(row + 1, col, numRows, numCols, memo) + self.countUniquePaths(row, col + 1, numRows, numCols, memo)
-            return memo[row][col]
+    def uniquePaths(self, m: int, n: int) -> int:
+        steps = m+n-2
+        right = m-1
+        down = n-1
+        ans = 1
 
-    def uniquePaths(self, numRows: int, numCols: int) -> int:
-        memo = [[-1 for _ in range(numCols)] for _ in range(numRows)]
-        return self.countUniquePaths(0, 0, numRows, numCols, memo)
+        for i in range(1,right+1):
+            ans = ans*(steps-right+i)/i
+        return int(ans)
+        
