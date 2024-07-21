45095fc0117f3e95338f68c81c35f94054593cdd
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-21 12:32:50 +0530

    Create bruteforceapproach.py

---
+++ 0493-reverse-pairs/bruteforceapproach.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] > 2 * nums[j]:
+                    count +=1
+
+        return count
