a51e1752c963e728af5798f0cf594b4e025d3351
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-22 18:18:21 +0530

    Update bruteforceapproach.py

--- 0018-4sum/bruteforceapproach.py
+++ 0018-4sum/bruteforceapproach.py
@@ -1,32 +1,18 @@
-
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        result = []
         n = len(nums)
+        r = []
+        unique_quadruplets = set()
         
-        for i in range(n - 3):
-           
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i + 1, n - 2):
-                
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                left, right = j + 1, n - 1
-                while left < right:
-                    four_sum = nums[i] + nums[j] + nums[left] + nums[right]
-                    if four_sum == target:
-                        result.append([nums[i], nums[j], nums[left], nums[right]])
-                       
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
-                        left += 1
-                        right -= 1
-                    elif four_sum < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return result
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    for l in range(k + 1, n):
+                        num = nums[i] + nums[j] + nums[k] + nums[l]
+                        if target == num:
+                            result = tuple(sorted([nums[i], nums[j], nums[k], nums[l]]))
+                            if result not in unique_quadruplets:
+                                unique_quadruplets.add(result)
+                                r.append(list(result))
+        
+        return r
