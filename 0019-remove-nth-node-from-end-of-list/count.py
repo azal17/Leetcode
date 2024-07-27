9a939f8899f881d5163f34e1687322bca7f27bc8
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-27 11:51:24 +0530

    Create count.py

---
+++ 0019-remove-nth-node-from-end-of-list/count.py
@@ -0,0 +1,26 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        q = dummy
+        count = 0
+
+        while q.next is not None:
+            count +=1
+            q = q.next
+        
+        actual_node_position = count - n
+        q = dummy
+
+        for _ in range(actual_node_position):
+            q = q.next
+        
+        q.next = q.next.next
+
+        return dummy.next
