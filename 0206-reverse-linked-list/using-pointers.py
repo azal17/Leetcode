11f538db670ba60760dd4990d7f8e5a0af05ea70
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-07-28 21:13:42 +0530

    Create using-pointers.py

---
+++ 0206-reverse-linked-list/using-pointers.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    class ListNode(object):
+        def __init__(self, val =0, next = None):
+            self.val = val
+            self.next = next
+    def reverseList(self, head):
+        q = head
+
+        prev = None
+
+        while q is not None:
+            front = q.next
+            q.next = prev
+            prev = q
+            q = front
+        return prev
