d819aa85537b53d83772acb64f88c2b02b7272fa
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-01 19:24:09 +0530

    Update reversing.py

--- 0002-add-two-numbers/without-reversing.py
+++ 0002-add-two-numbers/without-reversing.py
@@ -1,25 +1,42 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        temp = dummy
-        carry = 0
-        
-        while l1 is not None or l2 is not None or carry:
-            sum_val = carry
-            if l1 is not None:
-                sum_val += l1.val
-                l1 = l1.next
-            if l2 is not None:
-                sum_val += l2.val
-                l2 = l2.next
+        def reverseList(self,head):
+            if head is None or head.next is None:
+                return head
+
+            new_head = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+
+            return new_head
+
+        def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+            dummy = ListNode(0)
+            temp = dummy
+            p = self.reverseList(l1)
+            q = self.reverseList(l2)
+            carry = 0
             
-            carry = sum_val // 10
-            temp.next = ListNode(sum_val % 10)
-            temp = temp.next
+            while p is not None or q is not None or carry:
+                sum_val = 0
+                if p is not None:
+                    sum_val += p.val
+                    p = p.next
+                if q is not None:
+                    sum_val += q.val
+                    q = q.next
+
+                sum_val += carry 
+                carry = sum_val // 10
+                temp.next = ListNode(sum_val % 10)
+                temp = temp.next
+            
+            k = dummy.next
+            
         
-        return dummy.next
+            return k
