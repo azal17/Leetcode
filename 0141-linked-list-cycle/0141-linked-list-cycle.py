363037a0d1712e7bdf4a78eb2f14413ae998953e
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-04 17:42:08 +0530

    Time: 43 ms (72.22%), Space: 19 MB (53.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,3 +1,6 @@
+# Time Complexity: O(N),Space Complexity : O(1)
+# Tortoise and Hare Algorithm 
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -6,13 +9,14 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        temp = head
-        hash = {}
-
-        while temp is not None:
-            if temp in hash:
-                return True
-            hash[temp] = temp.val
-            temp = temp.next
-        return False
+        slow = head
+        fast = head
         
+        while fast is not None and fast.next != None: # has to be in loop cant be null
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        return False
+            
