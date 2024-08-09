3df72391869879ca91ab22de592fa4a84a31936b
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-09 12:54:13 +0530

    Create brute-force-approach.py

---
+++ 0026-remove-duplicates-from-sorted-array/brute-force-approach.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        s = set()
+        i = 0
+        
+        while i < len(nums):
+            if nums[i] in s:
+                nums.pop(i)  
+            else:
+                s.add(nums[i])
+                i += 1  
+        
+        return len(nums)
