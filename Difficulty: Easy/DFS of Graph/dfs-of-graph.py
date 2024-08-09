39d5a65c100d7cf34858b0407f3907ee363c2555
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-09 22:01:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    
+    def dfsOfGraph(self, V, adj):
+        dfs_traversal = []
+        visited = set()
+    
+        def dfs(v):
+            visited.add(v)
+            dfs_traversal.append(v)
+            for neighbor in adj[v]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        dfs(0)
+        
+        return dfs_traversal
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__':
+    T=int(input())
+    while T>0:
+        V,E=map(int,input().split())
+        adj=[[] for i in range(V+1)]
+        for i in range(E):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+        ob=Solution()
+        ans=ob.dfsOfGraph(V,adj)
+        for i in range(len(ans)):
+            print(ans[i],end=" ")
+        print()
+        T-=1
+# } Driver Code Ends
