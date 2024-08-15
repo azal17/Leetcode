274ec8c61d318d738e14e0435b3581bc1dbe4c76
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-15 18:56:10 +0530

    Create brute-force-approach.py

---
+++ 0042-trapping-rain-water/brute-force-approach.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        count = 0
+        for i in range(n):
+            j = i
+            leftMax = 0
+            rightMax = 0
+            while j >= 0:
+                leftMax = max(leftMax, height[j])
+                j -= 1
+            j = i
+            while j < n:
+                rightMax = max(rightMax, height[j])
+                j += 1
+            count = count+ (min(leftMax, rightMax) - height[i])
+        return count
