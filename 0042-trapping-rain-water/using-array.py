7e3b7f128f9b0000aa138b74de2819ca92acf2e5
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-15 18:58:17 +0530

    Create using-array.py

---
+++ 0042-trapping-rain-water/using-array.py
@@ -0,0 +1,18 @@
+# Time Complexity = O(3*n)-> 3 for loops , Space Complexity = O(2*n) -> 2 arrays
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        prefix, suffix = [0]*n , [0]*n
+        prefix[0] = height[0]
+
+        for i in range(1, n):
+            prefix[i] = max( prefix[i-1], height[i])
+        suffix[n-1] = height[n-1]
+
+        for i in range(n-2, -1, -1):
+            suffix[i] = max(suffix[i+1], height[i])
+        count = 0
+
+        for i in range(n):
+            count += min(prefix[i], suffix[i]) - height[i]
+        return count
