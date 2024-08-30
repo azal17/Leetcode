2f927715996e1a5d200724c1151c54190df6202d
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-08-30 18:50:10 +0530

    Time: 47 ms (82.22%), Space: 16.6 MB (34.29%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,37 +1,23 @@
-from typing import List, Tuple
-
 class Solution:
-    def __init__(self):
-        
-        self.dp = [[None] * 31 for _ in range(101)]
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]: 
-        def dfs(i: int, target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n =len(candidates)
+        candidates.sort()
+        ans, temp = [],[]
+        def cs(index: int, target: int):
             if target == 0:
-                return [[]]
-            if i == n or target < 0:
-                return []
-            if self.dp[i][target] is not None:
-                return self.dp[i][target]
-            
-            x, m = nWm[i]
-            result = []
-            for j in range(m + 1):
-                combinations = dfs(i + 1, target - j * x)
-                for comb in combinations:
-                    result.append([x] * j + comb)
-            
-            self.dp[i][target] = result
-            return result
-        freq = [0] * 51
-        xMax = 0
-        for x in candidates:
-            freq[x] += 1
-            xMax = max(xMax, x)
-        nWm = []
-        for x in range(1, xMax + 1):
-            f = min(freq[x], target // x)
-            if f > 0:
-                nWm.append((x, f))
-        n = len(nWm)
-        self.dp = [[None] * (target + 1) for _ in range(n)]
-        return dfs(0, target)
+                ans.append(temp[:])
+                return
+            if index == n:
+                return
+
+            for j in range(index, n):
+
+                if j > index and candidates[j] == candidates[j-1]:
+                    continue
+                if target < candidates[j]:
+                    break
+                temp.append(candidates[j])
+                cs(j+1, target - candidates[j])
+                temp.pop()
+        cs(0,target)
+        return ans
