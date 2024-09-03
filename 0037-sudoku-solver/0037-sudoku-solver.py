3035cdf8a75968f5389fd8b31031f390a9994bc2
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-03 18:45:22 +0530

    Time: 321 ms (22.69%), Space: 16.4 MB (99.49%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,42 +1,34 @@
-from typing import List
-
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        def isSafe(board: List[List[str]], row: int, col: int, num: str) -> bool:
- 
+        def isSafe(row: int, col: int, num, board: List[List[str]]):
+            for x in range(9):
+                if board[x][col] == num:
+                    return False
             for x in range(9):
                 if board[row][x] == num:
                     return False
 
-
-            for x in range(9):
-                if board[x][col] == num:
-                    return False
-
-
-            startRow = row - row % 3
-            startCol = col - col % 3
+            start_row = row - row%3
+            start_col= col - col%3
             for i in range(3):
                 for j in range(3):
-                    if board[i + startRow][j + startCol] == num:
+                    if board[ i + start_row ][j + start_col] == num:
                         return False
-
             return True
 
-        def solve(board: List[List[str]]) -> bool:
-            
-            for row in range(9):
-                for col in range(9):
-                    if board[row][col] == '.':
+
+        def solve(board: List[List[str]] ):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j] == ".":
                         for num in map(str, range(1, 10)):
-                            if isSafe(board, row, col, num):
-                                board[row][col] = num
-                                if solve(board):
+                            if isSafe(i,j,num,board):
+                                board[i][j] = num
+                                if solve(board): 
                                     return True
-                                board[row][col] = '.'
-
+                                board[i][j] = "."
                         return False
-
             return True
-
         solve(board)
+
+        
