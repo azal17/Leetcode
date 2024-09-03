8d18c516e08ef64432aa45a977d4b07082da7bba
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-03 19:17:39 +0530

    Time: 352 ms (12.21%), Space: 16.7 MB (43.79%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,20 +1,18 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         def isSafe(row: int, col: int, num, board: List[List[str]]):
+            
+            
             for x in range(9):
                 if board[x][col] == num:
                     return False
-            for x in range(9):
                 if board[row][x] == num:
                     return False
-
-            start_row = row - row%3
-            start_col= col - col%3
-            for i in range(3):
-                for j in range(3):
-                    if board[ i + start_row ][j + start_col] == num:
-                        return False
+                if board[3*(row//3) + x//3][3*(col//3) + x%3] == num:
+                    return False
             return True
+
+
 
 
         def solve(board: List[List[str]] ):
