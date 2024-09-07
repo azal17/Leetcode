a9b573379026120add02fa596fa3b049ccddace7
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-07 09:50:14 +0530

    Time: 71 ms (93.99%), Space: 16.9 MB (64.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,47 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n = len(nums1)
-        m = len(nums2)
-        k  = n + m
+        n, m = len(nums1), len(nums2)
+        if n > m:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        left = (n + m + 1) // 2 
+        low, high = 0, n
+        
+        while low <= high:
+            mid1 = (low + high) // 2
+            mid2 = left - mid1
+          
+            l1 = nums1[mid1 - 1] if mid1 > 0 else float('-inf')
+            r1 = nums1[mid1] if mid1 < n else float('inf')
+            l2 = nums2[mid2 - 1] if mid2 > 0 else float('-inf')
+            r2 = nums2[mid2] if mid2 < m else float('inf')
 
-        index2 = k // 2
-        index1 = index2 - 1
-        cnt = 0
-        index1_element, index2_element = -1, -1
-
-        i, j = 0, 0
-        while i < n and j < m:
-            if nums1[i] < nums2[j]:
-                if cnt == index1:
-                    index1_element = nums1[i]
-                if cnt == index2:
-                    index2_element = nums1[i]
-                cnt += 1
-                i += 1
+            if l1 <= r2 and l2 <= r1:
+                if (n + m) % 2 == 1:
+                    return max(l1, l2)
+                else:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+            elif l1 > r2:
+                high = mid1 - 1
             else:
-                if cnt == index1:
-                    index1_element = nums2[j]
-                if cnt == index2:
-                    index2_element = nums2[j]
-                cnt += 1
-                j += 1
-
-        while i < n:
-            if cnt == index1:
-                index1_element = nums1[i]
-            if cnt == index2:
-                index2_element = nums1[i]
-            cnt += 1
-            i += 1
-
-        while j < m:
-            if cnt == index1:
-                index1_element = nums2[j]
-            if cnt == index2:
-                index2_element = nums2[j]
-            cnt += 1
-            j += 1
-
-        if k % 2 == 1:
-            return index2_element
-        return (index1_element + index2_element) / 2.0
+                low = mid1 + 1
+        
+        return 0.0  
