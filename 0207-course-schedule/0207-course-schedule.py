f2d80ca6f74aef7495076c6a4c39c316f380a8b4
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-17 17:53:03 +0530

    Time: 112 ms (8.04%), Space: 18.2 MB (81.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,32 @@
+from queue import Queue
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+
         adj = {i: [] for i in range(numCourses)}
 
         for dependent, independent in prerequisites:
             adj[independent].append(dependent)
 
-        def dfs(node, visited, pathvis):
-            visited.add(node)
-            pathvis[node] = 1
+        vis = set()  
+        q = Queue() 
+        indegree = [0] * numCourses 
+        result = [] 
 
-
-            for neigh in adj[node]:
-                if neigh not in visited:
-                    if dfs(neigh, visited, pathvis):
-                        return True
-                elif pathvis[neigh] == 1:
-                    return True
-
-            pathvis[node] = 0
-            return False
-
-        visited = set()
-        pathvis = [0] * numCourses
         for i in range(numCourses):
-            if i not in visited:
-                if dfs(i, visited, pathvis):
-                    return False
-        return True
+            for n in adj[i]:
+                indegree[n] += 1 
+
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.put(n)
+
+        while not q.empty():
+            nodee = q.get()
+            result.append(nodee)
+
+            for j in adj[nodee]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.put(j)
+        return len(result) == numCourses
