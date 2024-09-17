f7fa56637781839d3b549a22db1b8445e933e5c1
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-17 18:33:04 +0530

    Create using-grid-directly.py

---
+++ 0200-number-of-islands/using-grid-directly.py
@@ -0,0 +1,29 @@
+#T.C, S.C = O(m*n) , wc = all elements in stack
+#dfs approach
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+
+        def markingislands(row, col):
+            stack = [(row, col)]
+
+            while stack:
+                x, y = stack.pop()
+                if 0 <= x < m and 0 <= y < n and grid[x][y] == '1':
+                    grid[x][y] = '0'  
+                    stack.append((x + 1, y))
+                    stack.append((x - 1, y))
+                    stack.append((x, y - 1))
+                    stack.append((x, y + 1))
+
+        islands = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':  
+                    islands += 1
+                    markingislands(i, j)
+
+        return islands
