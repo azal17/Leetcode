a12407833f05b2f17d3fe3f9e5ed83a39f650574
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-17 19:26:17 +0530

    Time: 235 ms (73.95%), Space: 18.8 MB (95.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,28 @@
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
 
         m, n = len(grid), len(grid[0])
-
-        def markingislands(row, col):
-            stack = [(row, col)]
-
-            while stack:
-                x, y = stack.pop()
-                if 0 <= x < m and 0 <= y < n and grid[x][y] == '1':
-                    grid[x][y] = '0'  
-                    stack.append((x + 1, y))
-                    stack.append((x - 1, y))
-                    stack.append((x, y - 1))
-                    stack.append((x, y + 1))
-
+        vis = [[0] * n for _ in range(m)]
         islands = 0
+
+        def dfs(row, col, vis, grid):
+            vis[row][col] = 1
+            delrow = [-1, 0, 1, 0]
+            delcol = [0, -1, 0, 1]
+
+            for i in range(4):
+                nrow = row + delrow[i]
+                ncol = col + delcol[i]
+
+                if 0 <= nrow < m and 0 <= ncol < n and vis[nrow][ncol] == 0 and grid[nrow][ncol] == '1':
+                    dfs(nrow, ncol, vis, grid)
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':  
+                if grid[i][j] == '1' and vis[i][j] == 0:
+                    dfs(i, j, vis, grid)
                     islands += 1
-                    markingislands(i, j)
 
         return islands
