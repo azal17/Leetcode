6f4d0203530605d75f624efb4ae5817b6f80a16a
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-17 17:59:07 +0530

    Time: 116 ms (7.64%), Space: 18.2 MB (81.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -12,7 +12,7 @@ class Solution:
         q = Queue() 
         indegree = [0] * numCourses 
         result = [] 
-
+        count = 0
         for i in range(numCourses):
             for n in adj[i]:
                 indegree[n] += 1 
@@ -24,9 +24,9 @@ class Solution:
         while not q.empty():
             nodee = q.get()
             result.append(nodee)
-
+            count +=1
             for j in adj[nodee]:
                 indegree[j] -= 1
                 if indegree[j] == 0:
                     q.put(j)
-        return len(result) == numCourses
+        return count == numCourses
