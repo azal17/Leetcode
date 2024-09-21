6d3a362b077b16c20e0661ab972600edbecc2274
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-21 19:10:10 +0530

    Create using-bfs.py

---
+++ 0785-is-graph-bipartite/using-bfs.py
@@ -0,0 +1,22 @@
+#T.C = O(V+E)
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        def dfs(node, color):
+            for n in graph[node]:
+                if colors[n] == -1:
+                    colors[n] = 1- colors[node]
+                    if not dfs(n,colors):
+                        return False
+                elif colors[n] == colors[node]:
+                    return False
+            return True
+
+        
+        n = len(graph)
+        colors = [-1]*n
+        for i in range(n):
+            if colors[i] == -1:
+                colors[i] = 0
+                if not dfs(i,colors):
+                    return False
+        return True      
