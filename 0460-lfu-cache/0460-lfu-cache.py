cf2236f012ba8c5a854b3f17edec4c9017b36dfe
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-09-26 17:33:48 +0530

    Time: 599 ms (40.97%), Space: 79.8 MB (93.71%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -2,42 +2,65 @@ class Node:
     def __init__(self, key: int, value: int):
         self.key = key
         self.value = value
-        self.next = None
+        self.count = 1  
         self.prev = None
+        self.next = None
+
+class List:
+    def __init__(self):
+        self.head = Node(0, 0)  
+        self.tail = Node(0, 0)  
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0 
+
+    def add_front(self, node: Node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+        self.size += 1
+
+    def remove(self, node: Node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        self.size -= 1
+
+    def pop_back(self) -> Node:
+        if self.size > 0:
+            last_node = self.tail.prev
+            self.remove(last_node)
+            return last_node
+        return None
 
 class LFUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}  
-        self.key_freq = {}  
-        self.freq_list = {} 
-        self.min_freq = 0 
+        self.freq_map = {}  
+        self.min_freq = 0  
+        self.curr_size = 0  
 
-    def addnode(self, node: Node, freq: int):
-        if freq not in self.freq_list:
-            self.freq_list[freq] = DoublyLinkedList()
-        self.freq_list[freq].add_to_front(node)
-
-    def delnode(self, node: Node, freq: int):
-        self.freq_list[freq].remove(node)
-        if self.freq_list[freq].is_empty():
-            del self.freq_list[freq]
+    def update_freq(self, node: Node):
+        freq = node.count
+        self.freq_map[freq].remove(node)
+        if self.freq_map[freq].size == 0:
+            del self.freq_map[freq]
             if self.min_freq == freq:
                 self.min_freq += 1
+        node.count += 1
+        new_freq = node.count
+        if new_freq not in self.freq_map:
+            self.freq_map[new_freq] = List()
+        self.freq_map[new_freq].add_front(node)
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-
         node = self.cache[key]
-        freq = self.key_freq[key]
-
-        self.delnode(node, freq)
-
-        self.key_freq[key] = freq + 1
-        self.addnode(node, freq + 1)
-
+        self.update_freq(node)
         return node.value
 
     def put(self, key: int, value: int) -> None:
@@ -45,51 +68,29 @@ class LFUCache:
             return
 
         if key in self.cache:
-
             node = self.cache[key]
             node.value = value
-            freq = self.key_freq[key]
-            self.delnode(node, freq)
-            self.key_freq[key] = freq + 1
-            self.addnode(node, freq + 1)
+            self.update_freq(node)
         else:
-            if len(self.cache) >= self.capacity:
-    
-                lfu_node = self.freq_list[self.min_freq].pop_back()
+            if self.curr_size >= self.capacity:
+                lfu_list = self.freq_map[self.min_freq]
+                lfu_node = lfu_list.pop_back()
                 del self.cache[lfu_node.key]
-                del self.key_freq[lfu_node.key]
+                self.curr_size -= 1
+
 
             new_node = Node(key, value)
             self.cache[key] = new_node
-            self.key_freq[key] = 1
-            self.min_freq = 1
-            self.addnode(new_node, 1)
+            if 1 not in self.freq_map:
+                self.freq_map[1] = List()
+            self.freq_map[1].add_front(new_node)
+            self.min_freq = 1  
+            self.curr_size += 1
 
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
 
-    def add_to_front(self, node: Node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
 
-    def remove(self, node: Node):
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
 
-    def pop_back(self) -> Node:
-        if self.is_empty():
-            return None
-        lru_node = self.tail.prev
-        self.remove(lru_node)
-        return lru_node
-
-    def is_empty(self):
-        return self.head.next == self.tail
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)R
