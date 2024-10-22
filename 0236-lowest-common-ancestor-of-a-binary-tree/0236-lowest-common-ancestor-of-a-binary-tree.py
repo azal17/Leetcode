ffac0d20f6390c475cbc17552baaa10d27ea358b
Sheikh Azal Abid <143110429+azal17@users.noreply.github.com>
2024-10-22 20:28:18 +0530

    Time: 51 ms (50.30%), Space: 21.1 MB (37.02%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,25 +6,15 @@
 #         self.right = None
 
 class Solution:
-    def findPath(self, root, target, path):
-        if root is None:
-            return False
-        path.append(root)
-        if root == target:
-            return True
-        if (root.left and self.findPath(root.left, target, path)) or \
-        (root.right and self.findPath(root.right, target, path)):
-            return True
-        path.pop()
-        return False
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1, path2 = [], []
-
-        if not self.findPath(root, p, path1) or not self.findPath(root, q, path2):
-            return None
-        i = 0
-        while i < len(path1) and i < len(path2) and path1[i] == path2[i]:
-            i += 1
-        i -= 1
-        return path1[i] if i >= 0 else None
+        if root is None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left is None:
+            return right
+        elif right is None:
+            return left
+        else:
+            return root
+        
