9d3f8fe647b4c2b55843d5d128613bcf942f4656
azal <143110429+azal17@users.noreply.github.com>
2024-10-29 19:03:29 +0530

    Create stack.py

---
+++ 0114-flatten-binary-tree-to-linked-list/stack.py
@@ -0,0 +1,25 @@
+# T.C / S.C = O(N)
+#Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            if cur.right:
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+            if stack:
+                cur.right = stack[-1]
+            cur.left = None
