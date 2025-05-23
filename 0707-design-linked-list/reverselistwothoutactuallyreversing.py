aa323d40be95fc381f9a28fbca986c8757914b47
azal <143110429+azal17@users.noreply.github.com>
2025-05-23 12:11:07 +0530

    Create reverselistwothoutactuallyreversing.py

---
+++ 0707-design-linked-list/reverselistwothoutactuallyreversing.py
@@ -0,0 +1,16 @@
+# Iterative O(n) both.
+def rr(head):
+    q = head
+    arr = []
+    while q:
+        arr.append(q.val)
+        q = q.next
+    arr = arr[::-1]
+    print(arr)
+
+# Recursion O(n) both (uses stack).
+def rr1(head):
+    if head == None:
+        return
+    rr1(head.next)
+    print(head.val)
