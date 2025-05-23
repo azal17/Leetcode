7cd3fba4563ed3f148b281750ec37fcfcf821000
azal <143110429+azal17@users.noreply.github.com>
2025-05-23 18:22:43 +0530

    Time: 11 ms (72.54%), Space: 17.9 MB (48.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        mpp = {}
+        if len(s) != len(t):
+            return False
+        for i in s:
+            mpp[i] = mpp.get(i, 0) + 1
+        for j in t:
+            if j not in mpp:
+                return False
+            mpp[j] -= 1
+            if mpp[j] < 0:
+                return False
+        return True
+        
