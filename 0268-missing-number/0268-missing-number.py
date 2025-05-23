3778f5d9d0da7ae35249b7fd2d954968de0465a6
azal <143110429+azal17@users.noreply.github.com>
2025-05-23 12:35:25 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        mpp = {i: 0 for i in range(n + 1)}  
-
-        for i in nums:
-            mpp[i] = 1
-
-        for i in range(n + 1):
-            if mpp[i] == 0:
-                return i
+        summ = (n*(n+1)) // 2
+        actual = sum(nums)
+        return summ - actual
